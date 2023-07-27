b6819cdc7e121c6445af4a2fe8beb74cd31a93f2
Muhammadhassan63 <hassan.malikk63@gmail.com>
2023-07-27 15:36:52 +0500

    hashing email and password done

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-
 const app = express();
+const { sha512 } = require("js-sha512");
 const secretKey = "secretkey";
 const PORT = 8080;
 
@@ -12,8 +12,13 @@ app.use(express.json());
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
+  // Hash the email and password using SHA-512
+  const hashedEmail = sha512(email);
+  const hashedPassword = sha512(password);
+  
+
   // You can implement your own logic to check the credentials here
-  if (email === "test@example.com" && password === "password1") {
+  if (hashedEmail === "ab901d0112b7f85a9520651cc01864dc60620f74dd92a95db1f199fa4e579f3ae1db2d33cb7ce315e9d87cbc40f275a625b875810f3e229c4eb75241803334c0" && hashedPassword === "c9728f839e5d279f357e81a097df6fcad9d89b319060a3c1aac889c96ab2ad229bdfde0dce430714ac53b7f82ebb1da524b22be98eef7205653f4649d4757599") {
     const user = { email: email };
     const token = jwt.sign({ user }, secretKey, { expiresIn: "100s" });
     res.json({ token });
